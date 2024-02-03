9ae01c2ae25962a4c11f166844a24e999e5f1c6b
poliao <119989213+poliao@users.noreply.github.com>
2024-02-03 22:26:17 +0700

    .

--- botnoy.py
+++ botnoy.py
@@ -1,15 +1,12 @@
 from uiautomator2 import connect
 from PIL import ImageDraw, Image
-
+import subprocess
 # กำหนด IP ของ MuMu
-<<<<<<< HEAD
-emu_ip = "127.0.0.1:16384"
-=======
-emu_ip = "127.0.0.1:7555"
->>>>>>> a03c2f0c9a2de3765c29441b0738a60bd5f3cd05
+emu_ip = "127.0.0.1:16448"
 
 # เปิดการเชื่อมต่อกับ MuMu
-d = connect('<emu_ip>')
+d = connect(emu_ip)
+
 
 # กำหนดขนาดและตำแหน่งที่ต้องการดึงสี
 region_left = 200
@@ -20,14 +17,15 @@ region_bottom = 400
 # กำหนดสีที่ต้องการตรวจสอบ (RGB format)
 # ตั้งค่าให้เป็นช่วงสีดำถึงสีเทา
 target_color_start = (0, 0, 0)  # สีดำ
-target_color_end = (128, 128, 128)  # สีเทา
+target_color_end = (28,26,31)  # สีเทา
 
 # กำหนดตำแหน่งที่ต้องการ click หลังจากพบสี
-click_position = (100, 100)  # ตำแหน่ง x, y ที่ต้องการ click
+click_position = (1370, 704)  # ตำแหน่ง x, y ที่ต้องการ click
 
+color_found = False
 # ทดสอบตรวจสอบสี 20 ครั้ง
 for iteration in range(1, 200):
-    print(f"\nIteration {iteration}:")
+    # print(f"\nIteration {iteration}:")
 
     # ดึงรูปภาพจากพื้นที่ที่กำหนด
     screenshot_full = d.screenshot()
@@ -35,17 +33,20 @@ for iteration in range(1, 200):
     screenshot = screenshot_full.crop((region_left, region_top, region_right, region_bottom))
 
     # Save the captured image for reference
-    screenshot.save(f"screenshot_iteration_{iteration}.png")
-    screenshot.show()
-    
+    # screenshot.save(f"screenshot_iteration_{iteration}.png")
+    # screenshot.show()
+    # ดึงค่าสีที่ต้องการตรวจสอบจากรูปภาพ
     # ดึงสีของทุก pixel ในพื้นที่
     all_colors = list(screenshot_full.crop((region_left, region_top, region_right, region_bottom)).getdata())
 
     for color in all_colors:
         if all(start <= value <= end for start, value, end in zip(target_color_start, color, target_color_end)):
-            print("Color found in the specified range (between black and gray)")
             d.click(click_position[0], click_position[1])
             print("Color found in the specified range (between black and gray)")
+            color_found = True
+            break  # หยุดการทำงานทันทีหลังจากพบสี
 
+    if color_found:
+        break  # หยุดลูปทั้งหมดหลังจากพบสี
     else:
         print("No color found in the specified range")
